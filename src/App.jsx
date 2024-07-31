e75639efc071345d84cb024b773b6c11608634fe
dani <dan068@live.com>
2024-07-30 18:15:07 -0600

    config change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 
+/*https://cataas.com/cat/says/hello
+https://catfact.ninja/fact
+*/
 export function App () {
   const [fact, setFact] = useState('lorem ipsum cat fact whatever')
   return (
